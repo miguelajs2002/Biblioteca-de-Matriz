56c2f2f4f5b2082ef24fdda59eb0d33be046916f
miguelajs2002 <2203066846@cua.uam.mx>
2021-08-29 15:27:14 -0500

    2da version Matrices

---
+++ Lib/src/Matrices.c
@@ -0,0 +1,62 @@
+#include "../include/lib.h"
+#include <stdio.h>
+#include <conio.h>
+
+	int F;
+	int C;
+	
+void Matriz_A()
+{
+	printf("--MatrizA--\n\n");
+	int MatrizA[F][C], i,j, vector [F];
+		for(i=0; i<F; i++)
+			for(j=0; j<C; j++)
+			{
+			printf("Ingrese el elemento [%d][%d]: ", i,j);
+			scanf("%d", &MatrizA[i][j]);
+			vector[i]=vector[i]+MatrizA[i][j];
+			}
+		printf("La MatrizA generada es:\n\n");
+			for(i=0; i<F; i++)
+			{
+				for(j=0; j<C; j++)
+				{
+				printf("[%d]", MatrizA[i][j]);
+				}
+			printf("\n\n");
+			}
+		}
+		
+void Matriz_B()
+{
+	printf("--MatrizB--\n\n");
+	int MatrizB[F][C], i,j, vector [F];
+		for(i=0; i<F; i++)
+			for(j=0; j<C; j++)
+			{
+			printf("Ingrese el elemento [%d][%d]: ", i,j);
+			scanf("%d", &MatrizB[i][j]);
+			vector[i]=vector[i]+MatrizB[i][j];
+			}
+		printf("La MatrizB generada es:\n\n");
+			for(i=0; i<F; i++)
+			{
+				for(j=0; j<C; j++)
+				{
+				printf("[%d]", MatrizB[i][j]);
+				}
+			printf("\n");
+			}
+		}
+
+main(){
+	printf("Fila:\n");
+	scanf("%d", &F);
+	printf("Columna:\n");
+	scanf("%d", &C);
+
+	Matriz_A();
+	Matriz_B();
+	getch();
+	return 0;
+}
